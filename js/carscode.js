b33ba5d70dfbd83eb1763052fa001d54266d205a
Rengar0 <w437317@163.com>
2019-12-12 18:14:27 +0800

    代码第一次提交

---
+++ js/carscode.js
@@ -0,0 +1,652 @@
+var s = [{
+	"carLevel": "S",
+	"itemName": "终极黑域电摩",
+	"itemId": "101885"
+},{
+	"carLevel": "S",
+	"itemName": "双子",
+	"itemId": "100049"
+},{
+	"carLevel": "S",
+	"itemName": "净坛使者",
+	"itemId": "100048"
+},{
+	"carLevel": "S",
+	"itemName": "爆天雷诺",
+	"itemId": "98830"
+},{
+	"carLevel": "S",
+	"itemName": "海洋之神",
+	"itemId": "98829"
+},{
+	"carLevel": "S",
+	"itemName": "黑曼巴",
+	"itemId": "98263"
+},{
+	"carLevel": "S",
+	"itemName": "终极银天使",
+	"itemId": "91957"
+},{
+	"carLevel": "S",
+	"itemName": "众神之神",
+	"itemId": "90945"
+},{
+	"carLevel": "S",
+	"itemName": "终极烈焰新星",
+	"itemId": "89936"
+},{
+	"carLevel": "S",
+	"itemName": "终极爆天甲",
+	"itemId": "88865"
+},{
+	"carLevel": "S",
+	"itemName": "疾影M18",
+	"itemId": "86998"
+},{
+	"carLevel": "S",
+	"itemName": "终极紫电神驹",
+	"itemId": "85949"
+},{
+	"carLevel": "S",
+	"itemName": "玉影·疾风",
+	"itemId": "85948"
+},{
+	"carLevel": "S",
+	"itemName": "祥瑞·流火",
+	"itemId": "85947"
+},{
+	"carLevel": "S",
+	"itemName": "青瓷·幻想",
+	"itemId": "85946"
+},{
+	"carLevel": "S",
+	"itemName": "暗影龙骑",
+	"itemId": "84869"
+},{
+	"carLevel": "S",
+	"itemName": "终极变形销魂",
+	"itemId": "83042"
+},{
+	"carLevel": "S",
+	"itemName": "雪域冰魄",
+	"itemId": "82249"
+},{
+	"carLevel": "S",
+	"itemName": "黑域电魔",
+	"itemId": "81293"
+},{
+	"carLevel": "S",
+	"itemName": "雷瑟之光",
+	"itemId": "80542"
+},{
+	"carLevel": "S",
+	"itemName": "地狱火",
+	"itemId": "80161"
+},{
+	"carLevel": "S",
+	"itemName": "毁灭者",
+	"itemId": "78296"
+},{
+	"carLevel": "S",
+	"itemName": "终极猛兽",
+	"itemId": "77236"
+},{
+	"carLevel": "S",
+	"itemName": "终极针尖王者",
+	"itemId": "75808"
+},{
+	"carLevel": "S",
+	"itemName": "终极鬼战刀",
+	"itemId": "74363"
+},{
+	"carLevel": "S",
+	"itemName": "爆天甲",
+	"itemId": "74362"
+},{
+	"carLevel": "S",
+	"itemName": "斗战胜佛",
+	"itemId": "73697"
+},{
+	"carLevel": "S",
+	"itemName": "x先驱者",
+	"itemId": "72741"
+},{
+	"carLevel": "S",
+	"itemName": "异形杀戮",
+	"itemId": "71588"
+},{
+	"carLevel": "S",
+	"itemName": "终级麦凯伦",
+	"itemId": "70047"
+},{
+	"carLevel": "S",
+	"itemName": "终极暴风雪",
+	"itemId": "68643"
+},{
+	"carLevel": "S",
+	"itemName": "至尊王者剃刀",
+	"itemId": "66600"
+},{
+	"carLevel": "S",
+	"itemName": "巅峰战刀",
+	"itemId": "65864"
+},{
+	"carLevel": "S",
+	"itemName": "银凌闪电",
+	"itemId": "65109"
+},{
+	"carLevel": "S",
+	"itemName": "金焰战神",
+	"itemId": "63702"
+},{
+	"carLevel": "S",
+	"itemName": "赤道极光",
+	"itemId": "63431"
+},{
+	"carLevel": "S",
+	"itemName": "暗夜幽灵",
+	"itemId": "62772"
+},{
+	"carLevel": "S",
+	"itemName": "炼狱使者",
+	"itemId": "60430"
+},{
+	"carLevel": "S",
+	"itemName": "闪电风暴",
+	"itemId": "34851"
+},{
+	"carLevel": "S",
+	"itemName": "御剑游侠",
+	"itemId": "33789"
+},{
+	"carLevel": "S",
+	"itemName": "末日使者",
+	"itemId": "33788"
+},{
+	"carLevel": "S",
+	"itemName": "修罗战神",
+	"itemId": "38258"
+},{
+	"carLevel": "S",
+	"itemName": "海神之刺",
+	"itemId": "32819"
+},{
+	"carLevel": "S",
+	"itemName": "绝影神驹",
+	"itemId": "32407"
+},{
+	"carLevel": "S",
+	"itemName": "黄蜂战神",
+	"itemId": "32406"
+},{
+	"carLevel": "S",
+	"itemName": "绿影幽灵",
+	"itemId": "31570"
+},{
+	"carLevel": "S",
+	"itemName": "速度之魔",
+	"itemId": "30808"
+},{
+	"carLevel": "S",
+	"itemName": "锋速",
+	"itemId": "28779"
+},{
+	"carLevel": "S",
+	"itemName": "烈焰新星",
+	"itemId": "28110"
+},{
+	"carLevel": "S",
+	"itemName": "啸风绝影",
+	"itemId": "27598"
+},{
+	"carLevel": "S",
+	"itemName": "雷神之怒",
+	"itemId": "26931"
+},{
+	"carLevel": "S",
+	"itemName": "至尊帝王",
+	"itemId": "26144"
+},{
+	"carLevel": "S",
+	"itemName": "龙胆逆鳞",
+	"itemId": "25717"
+},{
+	"carLevel": "S",
+	"itemName": "时空战舰",
+	"itemId": "24924"
+},{
+	"carLevel": "S",
+	"itemName": "风之使者",
+	"itemId": "24410"
+},{
+	"carLevel": "S",
+	"itemName": "幽浮",
+	"itemId": "24146"
+},{
+	"carLevel": "S",
+	"itemName": "猛兽",
+	"itemId": "23980"
+},{
+	"carLevel": "S",
+	"itemName": "十字先锋",
+	"itemId": "23878"
+},{
+	"carLevel": "S",
+	"itemName": "异域使者",
+	"itemId": "23063"
+},{
+	"carLevel": "S",
+	"itemName": "暗夜极光",
+	"itemId": "23015"
+},{
+	"carLevel": "S",
+	"itemName": "紫电神驹",
+	"itemId": "22526"
+},{
+	"carLevel": "S",
+	"itemName": "鬼战刀",
+	"itemId": "22097"
+},{
+	"carLevel": "S",
+	"itemName": "巅峰黑夜",
+	"itemId": "21961"
+},{
+	"carLevel": "S",
+	"itemName": "星之精灵",
+	"itemId": "21593"
+},{
+	"carLevel": "S",
+	"itemName": "雾之战车",
+	"itemId": "21319"
+},{
+	"carLevel": "S",
+	"itemName": "诸神之剑",
+	"itemId": "21086"
+},{
+	"carLevel": "S",
+	"itemName": "沙皇",
+	"itemId": "20471"
+},{
+	"carLevel": "S",
+	"itemName": "通天晓",
+	"itemId": "20244"
+},{
+	"carLevel": "S",
+	"itemName": "针尖王者",
+	"itemId": "19951"
+},{
+	"carLevel": "S",
+	"itemName": "雷蛇",
+	"itemId": "19618"
+},{
+	"carLevel": "S",
+	"itemName": "风怒",
+	"itemId": "19298"
+},{
+	"carLevel": "S",
+	"itemName": "麦凯伦",
+	"itemId": "18767"
+},{
+	"carLevel": "S",
+	"itemName": "S黄金追魂者",
+	"itemId": "18179"
+},{
+	"carLevel": "S",
+	"itemName": "审判者",
+	"itemId": "17668"
+},{
+	"carLevel": "S",
+	"itemName": "天机线",
+	"itemId": "17354"
+},{
+	"carLevel": "S",
+	"itemName": "逐魂者",
+	"itemId": "16572"
+},{
+	"carLevel": "S",
+	"itemName": "追魂者",
+	"itemId": "15456"
+},{
+	"carLevel": "S",
+	"itemName": "S黄金剃刀",
+	"itemId": "14107"
+},{
+	"carLevel": "S",
+	"itemName": "S黑金剃刀",
+	"itemId": "14106"
+},{
+	"carLevel": "S",
+	"itemName": "圣骑士",
+	"itemId": "13902"
+},{
+	"carLevel": "S",
+	"itemName": "S白金剃刀",
+	"itemId": "13703"
+},{
+	"carLevel": "S",
+	"itemName": "风行者",
+	"itemId": "12948"
+},{
+	"carLevel": "S",
+	"itemName": "暴风雪",
+	"itemId": "11376"
+}]
+
+
+
+var t3 = [{
+	"carLevel": "T3",
+	"itemName": "圣域大天使",
+	"itemId": "85942"
+},{
+	"carLevel": "T3",
+	"itemName": "创世之神",
+	"itemId": "78292"
+},{
+	"carLevel": "T3",
+	"itemName": "神谕天尊",
+	"itemId": "71452"
+},{
+	"carLevel": "T3",
+	"itemName": "终极幻影",
+	"itemId": "63429"
+}]
+
+
+
+var t2 = [{
+	"carLevel": "T2",
+	"itemName": "幽冥剑圣",
+	"itemId": "92841"
+},{
+	"carLevel": "T2",
+	"itemName": "霸天堡垒",
+	"itemId": "91958"
+},{
+	"carLevel": "T2",
+	"itemName": "吕布奉先",
+	"itemId": "90944"
+},{
+	"carLevel": "T2",
+	"itemName": "死神·阿努比斯",
+	"itemId": "89934"
+},{
+	"carLevel": "T2",
+	"itemName": "炎舞战姬",
+	"itemId": "88864"
+},{
+	"carLevel": "T2",
+	"itemName": "战争之王",
+	"itemId": "88684"
+},{
+	"carLevel": "T2",
+	"itemName": "啸天神君",
+	"itemId": "85944"
+},{
+	"carLevel": "T2",
+	"itemName": "极影风神",
+	"itemId": "85943"
+},{
+	"carLevel": "T2",
+	"itemName": "亡灵收割者",
+	"itemId": "84867"
+},{
+	"carLevel": "T2",
+	"itemName": "凌霜圣使",
+	"itemId": "83986"
+},{
+	"carLevel": "T2",
+	"itemName": "猎鹰战神",
+	"itemId": "83038"
+},{
+	"carLevel": "T2",
+	"itemName": "雷霆霸主",
+	"itemId": "82248"
+},{
+	"carLevel": "T2",
+	"itemName": "擎天战神",
+	"itemId": "81291"
+},{
+	"carLevel": "T2",
+	"itemName": "猎魂裁决",
+	"itemId": "80541"
+},{
+	"carLevel": "T2",
+	"itemName": "赤焰战神",
+	"itemId": "80160"
+},{
+	"carLevel": "T2",
+	"itemName": "黄金守护神",
+	"itemId": "78294"
+},{
+	"carLevel": "T2",
+	"itemName": "白金守护神",
+	"itemId": "78293"
+},{
+	"carLevel": "T2",
+	"itemName": "守护者",
+	"itemId": "77235"
+},{
+	"carLevel": "T2",
+	"itemName": "鬼武者",
+	"itemId": "75807"
+},{
+	"carLevel": "T2",
+	"itemName": "金甲狮王",
+	"itemId": "74884"
+},{
+	"carLevel": "T2",
+	"itemName": "孙悟空",
+	"itemId": "74361"
+},{
+	"carLevel": "T2",
+	"itemName": "深渊武士",
+	"itemId": "72739"
+},{
+	"carLevel": "T2",
+	"itemName": "刀锋神兵",
+	"itemId": "71454"
+},{
+	"carLevel": "T2",
+	"itemName": "怒魂",
+	"itemId": "71453"
+},{
+	"carLevel": "T2",
+	"itemName": "御皇铠甲",
+	"itemId": "70046"
+},{
+	"carLevel": "T2",
+	"itemName": "末界战神",
+	"itemId": "68642"
+},{
+	"carLevel": "T2",
+	"itemName": "零羽天使",
+	"itemId": "68095"
+},{
+	"carLevel": "T2",
+	"itemName": "机械战警",
+	"itemId": "66599"
+},{
+	"carLevel": "T2",
+	"itemName": "火焰独角兽",
+	"itemId": "63701"
+},{
+	"carLevel": "T2",
+	"itemName": "圣白独角兽",
+	"itemId": "68700"
+},{
+	"carLevel": "T2",
+	"itemName": "黑暗骑士",
+	"itemId": "63482"
+},{
+	"carLevel": "T2",
+	"itemName": "圣天使",
+	"itemId": "62771"
+},{
+	"carLevel": "T2",
+	"itemName": "紫焰战神",
+	"itemId": "33895"
+},{
+	"carLevel": "T2",
+	"itemName": "暗影刺客",
+	"itemId": "33894"
+},{
+	"carLevel": "T2",
+	"itemName": "铁甲巨龙",
+	"itemId": "32408"
+},{
+	"carLevel": "T2",
+	"itemName": "雷霆机甲",
+	"itemId": "30890"
+},{
+	"carLevel": "T2",
+	"itemName": "复仇天使",
+	"itemId": "30118"
+},{
+	"carLevel": "T2",
+	"itemName": "光明骑士",
+	"itemId": "27219"
+},{
+	"carLevel": "T2",
+	"itemName": "黑暗风暴",
+	"itemId": "27206"
+}]
+
+
+
+var t1 = [{
+	"carLevel": "T1",
+	"itemName": "凌云战机",
+	"itemId": "93573"
+},{
+	"carLevel": "T1",
+	"itemName": "圣剑骑士",
+	"itemId": "91959"
+},{
+	"carLevel": "T1",
+	"itemName": "针尖守卫",
+	"itemId": "89935"
+},{
+	"carLevel": "T1",
+	"itemName": "裂风机械兽",
+	"itemId": "88863"
+},{
+	"carLevel": "T1",
+	"itemName": "战地先锋",
+	"itemId": "88683"
+},{
+	"carLevel": "T1",
+	"itemName": "启灵守护神",
+	"itemId": "85945"
+},{
+	"carLevel": "T1",
+	"itemName": "侍武之魂",
+	"itemId": "84868"
+},{
+	"carLevel": "T1",
+	"itemName": "弑火刺客",
+	"itemId": "83039"
+},{
+	"carLevel": "T1",
+	"itemName": "刑天战斧",
+	"itemId": "81292"
+},{
+	"carLevel": "T1",
+	"itemName": "无影之刃",
+	"itemId": "80159"
+},{
+	"carLevel": "T1",
+	"itemName": "玄武之魂",
+	"itemId": "78911"
+},{
+	"carLevel": "T1",
+	"itemName": "至尊青龙",
+	"itemId": "78295"
+},{
+	"carLevel": "T1",
+	"itemName": "白虎战神",
+	"itemId": "76477"
+},{
+	"carLevel": "T1",
+	"itemName": "超能特警",
+	"itemId": "74885"
+},{
+	"carLevel": "T1",
+	"itemName": "绝地战士",
+	"itemId": "74360"
+},{
+	"carLevel": "T1",
+	"itemName": "超能英雄",
+	"itemId": "73696"
+},{
+	"carLevel": "T1",
+	"itemName": "神影之尊",
+	"itemId": "72740"
+},{
+	"carLevel": "T1",
+	"itemName": "激刃",
+	"itemId": "72277"
+},{
+	"carLevel": "T1",
+	"itemName": "圣枪",
+	"itemId": "71455"
+},{
+	"carLevel": "T1",
+	"itemName": "天姬",
+	"itemId": "69207"
+},{
+	"carLevel": "T1",
+	"itemName": "神龙骑士",
+	"itemId": "68094"
+},{
+	"carLevel": "T1",
+	"itemName": "紫铭",
+	"itemId": "66598"
+},{
+	"carLevel": "T1",
+	"itemName": "水晶先锋",
+	"itemId": "65859"
+},{
+	"carLevel": "T1",
+	"itemName": "暗夜王爵",
+	"itemId": "64879"
+},{
+	"carLevel": "T1",
+	"itemName": "银魂捷豹",
+	"itemId": "63705"
+},{
+	"carLevel": "T1",
+	"itemName": "闪电侠",
+	"itemId": "63430"
+},{
+	"carLevel": "T1",
+	"itemName": "噬魂金甲",
+	"itemId": "60429"
+},{
+	"carLevel": "T1",
+	"itemName": "绿影骑士",
+	"itemId": "34850"
+},{
+	"carLevel": "T1",
+	"itemName": "战魂",
+	"itemId": "33257"
+},{
+	"carLevel": "T1",
+	"itemName": "朱雀",
+	"itemId": "28778"
+},{
+	"carLevel": "T1",
+	"itemName": "铁拳",
+	"itemId": "27565"
+},{
+	"carLevel": "T1",
+	"itemName": "青蜂侠",
+	"itemId": "27520"
+},{
+	"carLevel": "T1",
+	"itemName": "沙暴",
+	"itemId": "27519"
+}]
+
+
+
